3043098fac739e714818591d17622071438838f3
Peng.Li <peng.li@rea-group.com>
2014-12-28 15:05:53 +0800

    Remove unnecessary test

--- src/test/scala/com/thoughtworks/pli/intellij/remotepair/server/ServerHandlerProviderSpec.scala
+++ src/test/scala/com/thoughtworks/pli/intellij/remotepair/server/ServerHandlerProviderSpec.scala
@@ -22,14 +22,6 @@ class ServerHandlerProviderSpec extends MySpecification {
       handler.channelInactive(context1)
       handler.clients.size === 0
     }
-    "broadcast common received event to other context of same project" in new ProtocolMocking {
-      client(context1, context2).createOrJoinProject("test")
-
-      client(context1).send(changeContentEventA1)
-
-      there was one(context2).writeAndFlush(changeContentEventA1.toMessage)
-      there was no(context1).writeAndFlush(changeContentEventA1.toMessage)
-    }
   }
 
 }
