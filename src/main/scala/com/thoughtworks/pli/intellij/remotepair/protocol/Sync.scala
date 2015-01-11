2a2bf2cc608c39c9c7b9401806925ba00706ec58
Peng.Li <peng.li@rea-group.com>
2015-01-11 17:05:22 +0800

    Remove some file checkings

--- src/main/scala/com/thoughtworks/pli/intellij/remotepair/protocol/Sync.scala
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/protocol/Sync.scala
@@ -1,6 +1,5 @@
 package com.thoughtworks.pli.intellij.remotepair.protocol
 
-import com.thoughtworks.pli.intellij.remotepair.ServerLogger
 import org.json4s.native.Serialization
 
 case object SyncFilesForAll extends PairEvent {
@@ -24,13 +23,6 @@ case class PairableFiles(fromClientId: String, toClientId: String, fileSummaries
 }
 
 case class MasterPairableFiles(fromClientId: String, toClientId: String, paths: Seq[String], diffFiles: Int) extends PairEvent {
-  // TODO: remove it later
-  val invalid = paths.filter(_.startsWith("/Users"))
-  if (invalid.nonEmpty) {
-    ServerLogger.info("!!!!!!!!!!!!!!!! Found invalid paths:")
-    invalid.foreach(ServerLogger.info)
-    throw new RuntimeException("!!!!!!!!!!! Found invalid paths")
-  }
   override def toJson = Serialization.write(this)
 }
 
