d00e04cdfbf0d39b64a7af38e3fa8a2e4a2bb5c0
Peng.Li <peng.li@rea-group.com>
2015-11-02 17:02:12 +0800

    Comment outdated tests

--- src/test/scala/com/thoughtworks/pli/intellij/remotepair/utils/AdjustedStringOpsSpec.scala
+++ src/test/scala/com/thoughtworks/pli/intellij/remotepair/utils/AdjustedStringOpsSpec.scala
@@ -35,17 +35,11 @@ class AdjustedStringOpsSpec extends Specification {
       "如果两个人添加的内容完全相等,则第二个人操作会被调整为空" in {
         StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(10, "abc"))) should beEmpty
       }
-      "如果第二个人添加的内容是第一个人的一部分,且从同一个位置添加,则第二个人的操作会被调整为空" in {
-        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(10, "ab"))) should beEmpty
-      }
-      "如果第二个人添加的内容比第一个人多,且从同一个位置添加,则相同部分会被省略" in {
-        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(10, "abcde"))) === Seq(Insert(13, "de"))
-      }
       "如果第二个人添加内容不属于前几种情况,且起点在第一个人的起点位置或后面,则起点需要调整" in {
-        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(10, "bc"))) === Seq(Insert(13, "bc"))
+        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(10, "ab"))) === Seq(Insert(13, "ab"))
       }
       "如果第二个人添加内容不属于前几种情况,且起点在第一个人的起点前面,则完全不需要调整" in {
-        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(9, "bc"))) === Seq(Insert(9, "bc"))
+        StringDiff.adjustLaterOps(Seq(Insert(10, "abc")), Seq(Insert(9, "ab"))) === Seq(Insert(9, "ab"))
       }
     }
     "如果第一个人是删除操作,第二个人是添加操作,则后者的内容不需要调整,但起点有可能调整" should {
@@ -69,7 +63,7 @@ class AdjustedStringOpsSpec extends Specification {
         StringDiff.adjustLaterOps(Seq(Insert(10, "bc")), Seq(Delete(5, 4))) === Seq(Delete(5, 4))
       }
       "如果后者的起点小于前者的起点,则起点不需要调整,但要避开前者添加的内容" in {
-        StringDiff.adjustLaterOps(Seq(Insert(10, "bc")), Seq(Delete(9, 5))) === Seq(Delete(9, 1), Delete(12, 4))
+        StringDiff.adjustLaterOps(Seq(Insert(10, "bc")), Seq(Delete(9, 5))) === Seq(Delete(9, 1), Delete(11, 4))
       }
     }
   }
@@ -77,17 +71,17 @@ class AdjustedStringOpsSpec extends Specification {
   "如果两人几乎同时对同一份文档做了操作,前者做了一个,后者做了多个,后者的操作可能需要调整,才能在前者之后应用到文档上" should {
     "前面是一个添加操作,后面是多个任意操作" in {
       StringDiff.adjustLaterOps(Seq(Insert(10, "bc")), Seq(Insert(8, "xy"), Delete(11, 5), Insert(20, "op"))) === Seq(
-        Insert(8, "xy"), Delete(13, 5), Insert(22, "op")
+        Insert(8, "xy"), Delete(11, 1), Delete(13, 4), Insert(22, "op")
       )
     }
     "前面是一个添加操作,后面的删除操作会分裂" in {
       StringDiff.adjustLaterOps(Seq(Insert(10, "bc")), Seq(Delete(9, 5), Insert(15, "xy"))) === Seq(
-        Delete(9, 1), Delete(12, 4), Insert(17, "xy")
+        Delete(9, 1), Delete(11, 4), Insert(17, "xy")
       )
     }
     "前面是一个删除操作,后面是多个任意操作" in {
       StringDiff.adjustLaterOps(Seq(Delete(10, 8)), Seq(Insert(8, "xy"), Delete(11, 5), Insert(20, "op"))) === Seq(
-        Insert(8, "xy"), Insert(17, "op")
+        Insert(8, "xy"), Delete(11, 1), Insert(16, "op")
       )
     }
   }
@@ -108,8 +102,13 @@ class AdjustedStringOpsSpec extends Specification {
   "如果两人几乎同时对同一份文档做了多个操作,后者的操作可能需要调整,才能在前者之后应用到文档上" in {
     // hope to improve it, to let the `xy` and `op` still be in the final result
     StringDiff.adjustLaterOps(
-      Seq(Insert(8, "xy"), Delete(11, 5), Insert(20, "op")),
-      Seq(Insert(3, "abc"), Delete(10, 8), Delete(16, 4), Insert(30, "mn"))
-    ) === Seq(Insert(3, "abc"), Delete(11, 4), Delete(17, 3), Delete(22, 1), Insert(33, "mn"))
+      Seq(Insert(8, "xy"), Delete(11, 5), Insert(22, "op")),
+      Seq(Insert(3, "abc"), Delete(10, 8), Delete(20, 4), Insert(30, "mn"))
+    ) === Seq(Insert(3, "abc"), Delete(10, 1), Delete(12, 2), Delete(24, 4), Insert(34, "mn"))
   }
+
+  // 12345678901234567890123456789012345678901234567890
+  // 12345678xy9_____567890123op456789012345678901234567890
+  // 123abc4567________678901____67890123456789mn01234567890
+  // 123abc4567_xy______567890123op456789012345678901234567890
 }
